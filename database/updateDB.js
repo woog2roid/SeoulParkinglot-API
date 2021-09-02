df92a9b638027b1192683dc72b110ae731eb633a
woog2roid <wooguijung@korea.ac.kr>
2021-09-02 14:51:46 +0000

    fix(updateDB): seoul api가 업데이트 되는 과정에서 주차장의 total count가 부정확했던 점을 고려함.

--- database/updateDB.js
+++ database/updateDB.js
@@ -6,37 +6,28 @@ const currentTime = new Date();
 
 const updateDB = async () => {
 	try {
-		//총 주차장 갯수 확인
-		const responseCount = await axios.get(
-			`http://openapi.seoul.go.kr:8088/${process.env.SEOUL_API_KEY}/json/GetParkInfo/1/2/`
-		);
-		if (responseCount.data.GetParkInfo.RESULT.CODE !== 'INFO-000') {
-			console.log(`${currentTime}에 서울 공영주차장 API 오류 발생`);
-			return;
-		}
-		const totalCount = responseCount.data.GetParkInfo.list_total_count;
-
-		//확인된 주차장 갯수로 1000개씩 잘라서 받아오기
-		let cnt, response, data="";
-		for (cnt = 0; cnt < totalCount / 1000; cnt++) {
-			response = await axios.get(
+		//data 받아오기 (1000개 단위로 끊어서)
+		let cnt = 0, data = "";
+		while(true) {
+			const response = await axios.get(
 				`http://openapi.seoul.go.kr:8088/${process.env.SEOUL_API_KEY}/json/GetParkInfo/${cnt * 1000 + 1}/${(cnt + 1) * 1000}/`
 			);
-			if(response.data.GetParkInfo !== undefined)
+			if (response.data.GetParkInfo !== undefined) {
 				data += JSON.stringify(response.data.GetParkInfo.row);
+				cnt++;
+			}
+			else if (cnt >= 14) {
+				//seoul api가 5분마다 업데이트 되는 과정에서 불안정함...
+				//최소 14000개 이상이므로 강제로 조건 추가
+				break;
+			}
 		}
-		response = await axios.get(
-			`http://openapi.seoul.go.kr:8088/${process.env.SEOUL_API_KEY}/json/GetParkInfo/${cnt * 1000 + 1}/${totalCount}`
-		);
-		if(response.data.GetParkInfo !== undefined)
-			data += JSON.stringify(response.data.GetParkInfo.row);
-
 		//.json 파일로 생성하기
 		fs.writeFile('DB.json', data, 'utf-8', (e) => {
 			if (e) {
 				console.log(e);
 			} else {
-				console.log(`${currentTime}에 업데이트 성공적으로 진행함`);
+				console.log(`${currentTime}에 DB.JSON 업데이트 성공적으로 진행함`);
 			}
 		});
 	} catch (e) {
